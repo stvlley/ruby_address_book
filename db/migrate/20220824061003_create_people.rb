25805e11dcfe41087e99697bde6d0f2244ec009b
Nick Garza <thenickgarza14@gmail.com>
2022-08-24 01:44:50 -0500

    added people ttable to the database from the migration

---
+++ db/migrate/20220824061003_create_people.rb
@@ -0,0 +1,15 @@
+class CreatePeople < ActiveRecord::Migration[7.0]
+  def change
+    create_table :people do |t|
+      t.string :salutation
+      t.string :first_name
+      t.string :middle_name
+      t.string :last_name
+      t.integer :ssn
+      t.date :birth_date
+      t.text :comment
+      t.references :contact, polyorphic: true
+      t.timestamps
+    end
+  end
+end
