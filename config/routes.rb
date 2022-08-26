03cc9132da1b13637e1ed99df95ce7bf377c997e
Nick Garza <thenickgarza14@gmail.com>
2022-08-26 00:37:43 -0500

    put the root path to the person index and also added nested routes from people to email, number, and addresses. Testing that out

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  resources :addresses
-  resources :numbers
-  resources :emails
-  resources :people
+
+  root "people#index"
+
+  resources :people do
+    resources :number
+    resources :emails
+    resources :addresses
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
