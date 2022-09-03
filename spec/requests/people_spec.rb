ed9a610dc7dfa6eb95d998439ff4e91f626a8ba3
Nick Garza <thenickgarza14@gmail.com>
2022-09-03 18:12:25 -0500

    testing to make sure when someone visits the home page they are required to log in or sign before accessing the the page to see contacts and add contacts

--- spec/requests/people_spec.rb
+++ spec/requests/people_spec.rb
@@ -19,12 +19,11 @@ RSpec.describe "People", type: :request do
 end
 
 RSpec.describe "People", type: :request do
-  user = User.first_or_create!(name: "Roger", email: "roger@test.com", password: "secret", password_confirmation: "secret")
-  person = Person.create!(user: user, first_name: 'James', last_name: 'Rodriguese')      
-
-  describe "GET /people/:id/edit" do
+   describe "GET /people/:id/edit" do
     it "gets the page to edit a perosn" do
-      
+     user = User.first_or_create!(name: "Roger", email: "roger@test.com", password: "secret", password_confirmation: "secret")
+     person = Person.create!(user: user, first_name: 'James', last_name: 'Rodriguese')      
+
       get "/people/#{person.id}/edit"
      
       expect(response).to have_http_status(302)
