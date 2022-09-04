624a9e73496ad68950054b4eb31b87d45798fdb6
Nick Garza <thenickgarza14@gmail.com>
2022-09-04 03:10:51 -0500

    added tests to for updating a person, address, email, and phone number

--- spec/features/email_spec.rb
+++ spec/features/email_spec.rb
@@ -49,3 +49,41 @@ RSpec.describe 'Creating a Email' do
         end
     end
 end
+
+RSpec.describe 'Updating a Email' do 
+    describe 'viewing the emails page requires you to be logged in' do 
+        it "after a successfull log in a user is then able to create a new contact and update a email" do
+            
+             visit(new_person_path)
+
+             fill_in('Name', with: "Bobby")
+             fill_in('Email', with: "email@email.com")
+             fill_in('Password', with: "password")
+             fill_in('Password confirmation', with: "password")
+
+             click_button("Create User")
+
+            click_link('New contact')
+
+            fill_in('First name', with: "Bobby")
+            fill_in('Middle name', with: "Middle")
+            fill_in('Last name', with: "Garza")
+             
+            click_button("Create Person")
+
+            find("#create-email").click
+
+            fill_in('Email', with: "email@test.com")
+            
+            click_button("Create Email")
+
+            find("#update-email").click
+
+            fill_in('Email', with: "emailupdate@test.com")
+
+            click_button("Update Email")
+
+            expect(page).to have_content("Email was successfully updated.")
+        end
+    end
+end
