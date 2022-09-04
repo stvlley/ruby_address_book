14615287ba204af0f8856521537235a8c2d17bd4
Nick Garza <thenickgarza14@gmail.com>
2022-09-04 02:35:40 -0500

    added tests for the email and number to to test a succesfull create form submission as well as authentication to make sure a user is logged in before being able to access those pages

---
+++ spec/features/address_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+
+RSpec.describe 'GET /people/:id/addresses/new' do 
+    describe 'A user is able to create an address for the person' do 
+        it "signs a user up then fills the form in and submits successfully " do
+
+         user = User.first_or_create!(name: "Roger", email: "roger@test.com", password: "secret", password_confirmation: "secret")
+         person = Person.create!(user: user, first_name: 'James', last_name: 'Rodriguese')      
+
+         visit("/people/#{person.id}/addresses/new")
+            
+         fill_in('Street', with: "123 abc st")
+         fill_in('Town', with: "austin")
+         fill_in('Zipcode', with: "12345")
+         
+         click_button("Create Address")
+
+         expect(page).to have_http_status(200)
+        end
+    end
+end
+
+RSpec.describe 'Creating a Address' do 
+    describe 'viewing the address page requires you to be logged in' do 
+        it "after a successfull log in a user is then able to create a new contact and addess" do
+            
+             visit(new_person_path)
+
+             fill_in('Name', with: "Bobby")
+             fill_in('Email', with: "email@email.com")
+             fill_in('Password', with: "password")
+             fill_in('Password confirmation', with: "password")
+
+             click_button("Create User")
+
+            click_link('New contact')
+
+            fill_in('First name', with: "Bobby")
+            fill_in('Middle name', with: "Middle")
+            fill_in('Last name', with: "Garza")
+             
+            click_button("Create Person")
+
+            find("#create-address").click
+
+            fill_in('Street', with: "123 abc st")
+            fill_in('Town', with: "austin")
+            fill_in('Zipcode', with: "12345")
+            
+            click_button("Create Address")
+
+            expect(page).to have_content("Address was successfully created.")
+        end
+    end
+end
